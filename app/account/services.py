16227255bd6ba04526f4621a3fb5c13c4ff5a3a6
Talha Rana <mtalharana093@gmail.com>
2025-12-04 19:50:18 +0500

    router file and service file setup

---
+++ app/account/services.py
@@ -0,0 +1,1 @@
+from app.account.models import User, RefreshToken
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.db.config import creat_tables
 from app.account.models import User, RefreshToken
+from app.account.routers import router as account_router
 
 
 @asynccontextmanager
@@ -12,6 +13,6 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/")
-def root():
-  return {"message" : "Server is running"}
+
+
+app.include_router(account_router)
